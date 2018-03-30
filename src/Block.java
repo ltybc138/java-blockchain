bcb460e6227166cf4f4ea0f26512d66411c8101d
ltybc138 <semenov200108@gmail.com>
2018-03-30 19:00:18 +0500

    First version of blockchain. Now we can generate hash of a block using SHA-256 encoding.

---
+++ src/Block.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculatedHash();
+    }
+
+    public String calculatedHash() {
+        String calculatedHash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        data
+        );
+        return calculatedHash;
+    }
+}
---
+++ src/DeniChain.java
@@ -0,0 +1,14 @@
+public class DeniChain {
+    public static void main(String[] args) {
+        Block firstBlock = new Block("First block", "0");
+        System.out.println("Hash for block 1 is : " + firstBlock.hash);
+
+
+        Block secondBlock = new Block("Second block", "0");
+        System.out.println("Hash for block 2 is : " + secondBlock.hash);
+
+
+        Block thirdBlock = new Block("Third block", "0");
+        System.out.println("Hash for block 3 is : " + thirdBlock.hash);
+    }
+}
