ddc7525c95bd80c1407d8a7e1e3e8d4581f1cec2
ltybc138 <semenov200108@gmail.com>
2018-03-31 13:48:27 +0500

    Added feature of mining blocks and checker if block is valid

--- src/DeniChain.java
+++ src/DeniChain.java
@@ -1,14 +1,58 @@
+import java.util.ArrayList;
+
 public class DeniChain {
+    public static ArrayList<Block> blocks = new ArrayList<>();
+    public static int difficulty = 6;
     public static void main(String[] args) {
-        Block firstBlock = new Block("First block", "0");
-        System.out.println("Hash for block 1 is : " + firstBlock.hash);
+        long timeStart = System.currentTimeMillis();
+        blocks.add(new Block("First block", "0"));
+        System.out.println("Trying to mine block 1... ");
+        blocks.get(0).mineBlock(difficulty);
 
+        blocks.add(new Block("Second block", blocks.get(blocks.size() - 1).hash));
+        System.out.println("Trying to mine block 2... ");
+        blocks.get(1).mineBlock(difficulty);
 
-        Block secondBlock = new Block("Second block", firstBlock.hash);
-        System.out.println("Hash for block 2 is : " + secondBlock.hash);
+        blocks.add(new Block("Third block", blocks.get(blocks.size() - 1).hash));
+        System.out.println("Trying to mine block 3... ");
+        blocks.get(2).mineBlock(difficulty);
 
+        // printing all hashes of our blockchain
+//        int c = 1;
+//        for (Block block : blocks) {
+//            System.out.println("Hash for block " + (c++) + " : " + block.hash);
+//        }
+        System.out.println("Mining all blocks took : " + (System.currentTimeMillis() - timeStart) + " millis");
+    }
 
-        Block thirdBlock = new Block("Third block", secondBlock.hash);
-        System.out.println("Hash for block 3 is : " + thirdBlock.hash);
+    public static boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        // watch through all blocks
+        for (int i = 1; i < blocks.size(); i++) {
+            currentBlock = blocks.get(i);
+            previousBlock = blocks.get(i-1);
+
+            // compare registered hash and calculated hash
+            if (!currentBlock.previousHash.equals(previousBlock.hash)) {
+                System.out.println("Current hashes are not equal");
+                return false;
+            }
+
+            // compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous hashes are not equal");
+                return false;
+            }
+
+            // check if hash is calculated
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
+        }
+        return true;
     }
 }
